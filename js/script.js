8a747dbc539d37a46168c6ffc6e3d8e7c3e9e61a
SajaSwalgah <saja.swalgah@yahoo.com>
2019-09-25 22:12:19 +0300

    edit

---
+++ js/script.js
@@ -0,0 +1,20 @@
+  
+        var d = new Date();
+        alert("Today's date is " + d);
+        
+        var name = prompt("What is your name?");
+        alert("Hello, " + name + "!");
+        
+        var txt;
+var r = confirm("Go ahead and descover our webpage");
+if (r == true) {
+  txt = "You pressed OK!";
+} else {
+  txt = "You pressed cancel!";
+}
+
+
+
+
+       
+   
